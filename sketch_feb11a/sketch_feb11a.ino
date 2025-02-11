115608a54dd377cb30956e6639420c8c221c7583
shay gladden <saglad1@ilstu.edu>
2025-02-11 15:39:18 -0600

    Update sketch_feb11a.ino

--- sketch_feb11a/sketch_feb11a.ino
+++ sketch_feb11a/sketch_feb11a.ino
@@ -6,7 +6,7 @@ int buttonPin = 2;
 int buttonState = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  // the code that sets everything up
   pinMode(redPin, OUTPUT);
   pinMode(yellowPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
@@ -17,7 +17,7 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // this code will run repeatedly:
   buttonState = digitalRead(buttonPin);
  
   // Check if the button is pressed
@@ -27,26 +27,26 @@ void loop() {
     digitalWrite(yellowPin, LOW);
     digitalWrite(greenPin, HIGH);
   } else {
-    // Otherwise, cycle through the traffic light sequence
-    // Red light on, others off
+    // cycle through the traffic light sequence
+    // red light on, others off
     digitalWrite(redPin, HIGH);
     digitalWrite(yellowPin, LOW);
     digitalWrite(greenPin, LOW);
     delay(5000); // Red light stays on for 5 seconds
 
-    // Yellow light on, others off
+    // yellow light on, others off
     digitalWrite(redPin, LOW);
     digitalWrite(yellowPin, HIGH);
     digitalWrite(greenPin, LOW);
     delay(2000); // Yellow light stays on for 2 seconds
 
-    // Green light on, others off
+    // green light on, others off
     digitalWrite(redPin, LOW);
     digitalWrite(yellowPin, LOW);
     digitalWrite(greenPin, HIGH);
     delay(5000); // Green light stays on for 5 seconds
 
-    // Yellow light on before switching to red
+    // yellow light on before switching to red
     digitalWrite(redPin, LOW);
     digitalWrite(yellowPin, HIGH);
     digitalWrite(greenPin, LOW);
